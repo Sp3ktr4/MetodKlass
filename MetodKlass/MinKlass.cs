9ddfcbf06e25927b77f3a88ff09a7523dd6cef24
Spektra <bergman.william@elev.ga.ntig.se>
2023-05-22 16:36:46 +0200

    first commit

---
+++ MetodKlass/MinKlass.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class MinKlass
+{
+    public void MinMetod()
+    {
+        Console.WriteLine("Detta är min metod!");
+    }
+}
---
+++ MetodKlass/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MinKlass mittObjekt = new MinKlass();
+        mittObjekt.MinMetod();
+    }
+}
